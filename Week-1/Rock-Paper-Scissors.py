ec1417972ce28f4e31616e8b1f6d79729ac983bf
Nkamanyi <Chatutie.Nkamanyimartin@tuni.fi>
2023-04-24 19:18:57 +0300

    Initial commit

---
+++ Week-1/Rock-Paper-Scissors.py
@@ -0,0 +1,21 @@
+
+def main():
+    player_1 = input("Player 1, enter your choice (R/P/S): ")
+    player_2 = input("Player 2, enter your choice (R/P/S): ")
+
+    if player_1 == "P" and player_2 == "S":
+        print("Player 2 won!")
+    elif player_1 == "P" and player_2 == "R":
+        print("Player 1 won!")
+    elif player_1 == "R" and player_2 == "S":
+        print("Player 1 won!")
+    elif player_1 == "S" and player_2 == "P":
+        print("Player 1 won!")
+    elif player_1 == "R" and player_2 == "P":
+        print("Player 2 won!")
+    elif player_1 == "S" and player_2 == "R":
+        print("Player 2 won!")
+    else:
+        print("It's a tie!")
+
+main()
---
+++ Week-1/main_function.py
@@ -0,0 +1,7 @@
+
+def main():
+    name = input("Tell your name: ")
+    print(f"Hi {name}")
+    print("your coding skills are great!")
+
+main()
